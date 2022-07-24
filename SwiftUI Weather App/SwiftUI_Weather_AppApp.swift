ce90279ed51eb1291a0930d572fa3bb5fbec8596
SergeiTsybulya <sergtsybulya@gmail.com>
2022-07-24 22:56:18 +0300

    Initial Commit

---
+++ SwiftUI Weather App/SwiftUI_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_AppApp.swift
+//  SwiftUI Weather App
+//
+//  Created by Sergei Tsybulya on 24.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI Weather App/Utils/Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Extensions.swift
+//  SwiftUI Weather App
+//
+//  Created by Sergei Tsybulya on 24.07.2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = LinearGradient(gradient: Gradient(colors: [Color("Background 1"), Color("Background 2")]), startPoint: .topLeading, endPoint: .bottomTrailing)
+}
